5c2fa3a126545c6503fc9403e19d284df8f5692d
Anik Sur <62329547+Anik-Sur@users.noreply.github.com>
2022-07-19 19:48:58 +0530

    added login register and settings pages

--- src/App.js
+++ src/App.js
@@ -2,12 +2,20 @@ import Home from "./pages/home/Home";
 import TopBar from "./components/topbar/TopBar";
 import Single from "./pages/single/Single";
 import Write from "./pages/write/Write";
+import Settings from "./pages/settings/Settings";
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
 
 function App() {
   return (
     <div>
       <TopBar />
       <Home />
+      <Single />
+      <Write />
+      <Settings />
+      <Login />
+      <Register />
     </div>
   );
 }
