997745a6093a18a305d17d5028ee18936fd77cc3
Anik Sur <62329547+Anik-Sur@users.noreply.github.com>
2022-07-20 23:12:31 +0530

    added routing

--- src/App.js
+++ src/App.js
@@ -5,18 +5,22 @@ import Write from "./pages/write/Write";
 import Settings from "./pages/settings/Settings";
 import Login from "./pages/login/Login";
 import Register from "./pages/register/Register";
+import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
 
 function App() {
+  const user = true;
   return (
-    <div>
+    <Router>
       <TopBar />
-      <Home />
-      <Single />
-      <Write />
-      <Settings />
-      <Login />
-      <Register />
-    </div>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/register" element={user ? <Home /> : <Register />} />
+        <Route path="/login" element={user ? <Home /> : <Login />} />
+        <Route path="/write" element={user ? <Write /> : <Register />} />
+        <Route path="/settings" element={user ? <Settings /> : <Register />} />
+        <Route path="/post/:postId" element={<Single />} />
+      </Routes>
+    </Router>
   );
 }
 
